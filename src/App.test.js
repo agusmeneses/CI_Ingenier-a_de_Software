4a2b0e8bda239e01298a29a858394aa08058c625
Agustín Meneses <agusmeneses0@gmail.com>
2024-06-02 11:27:40 -0300

    Cambio de librerías en test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import { act } from 'react';
 import App from './App';
 
 test('elemento de input', () => {
-  render(<App />);
+  act(() => {
+    render(<App />);
+  });
   const inputElement = screen.getByPlaceholderText(/Ingrese un número del 1 al 100/i);
   expect(inputElement).toBeInTheDocument();
 });
+
