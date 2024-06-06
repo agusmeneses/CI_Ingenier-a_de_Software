83606db6fde40244e487dba89020993be24ea197
Agustín Meneses <agusmeneses0@gmail.com>
2024-06-06 19:51:09 -0300

    Agregado de tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { act } from 'react';
 import App from './App';
 
-test('elemento de input', () => {
+test('valor ingresado no es cero ni menor a cero', () => {
   act(() => {
     render(<App />);
   });
   const inputElement = screen.getByPlaceholderText(/Ingrese un número del 1 al 100/i);
-  expect(inputElement).toBeInTheDocument();
+  fireEvent.change(inputElement, { target: { value: '0' } });
+  fireEvent.click(screen.getByText(/Crear Ruleta/i));
+  expect(window.alert).toHaveBeenCalledWith('Por favor, ingrese un número del 1 al 100');
+});
+
+test('valor ingresado no es 101 ni mayor', () => {
+  act(() => {
+    render(<App />);
+  });
+  const inputElement = screen.getByPlaceholderText(/Ingrese un número del 1 al 100/i);
+  fireEvent.change(inputElement, { target: { value: '101' } });
+  fireEvent.click(screen.getByText(/Crear Ruleta/i));
+  expect(window.alert).toHaveBeenCalledWith('Por favor, ingrese un número del 1 al 100');
 });
 
